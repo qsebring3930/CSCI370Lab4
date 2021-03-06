4f5da034e2af7dd7181b5d5bb2940e90e35da397
thoopchuk26 <thoopchuk@gmail.com>
2021-03-06 15:12:30 -0600

    Added a FollowCam
    Found a good follow cam online, I put the credits for it in a text doc in the git

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class cameraFollow : MonoBehaviour
 {
+    public GameObject player;
+    private Vector3 offset;
+    private float newXPosition;
+    private float newZPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+
     }
 }
