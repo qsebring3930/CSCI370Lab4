b41cc2476fecb34858b566493d0429297f750d35
thoopchuk26 <thoopchuk@gmail.com>
2021-03-06 13:56:32 -0600

    Added Very Basic Keyboard Movement

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 2f;
+    private Rigidbody body;
+    private float horizontal;
+    private float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        body.AddForce(movement * speed);
+    }
+}
