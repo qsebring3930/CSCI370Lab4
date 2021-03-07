276771e2cc8c925c4eedb5bf3372ca7a219dc818
thoopchuk26 <thoopchuk@gmail.com>
2021-03-07 13:35:30 -0600

    Minor Movement Changes

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -24,14 +24,13 @@ public class movement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
-        if (Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
         {
             body.AddRelativeForce(vertical*(Vector3.forward*speed));
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
             {
-                body.AddTorque(horizontal * (Vector3.up * turnSpeed));
+                body.AddTorque(horizontal * (Vector3.up * turnSpeed), ForceMode.VelocityChange);
             }
         }
-        
     }
 }
