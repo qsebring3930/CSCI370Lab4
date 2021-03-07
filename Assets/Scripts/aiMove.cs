55da48daf8e449e861a713a4b2d6881034bfefed
Will Harris <william.harris108@gmail.com>
2021-03-07 13:57:43 -0600

    Made Ai move through rigidbody instead of navmeshagent

--- Assets/Scripts/aiMove.cs
+++ Assets/Scripts/aiMove.cs
@@ -6,17 +6,28 @@ using UnityEngine.AI;
 public class aiMove : MonoBehaviour
 {
     public Transform goal;
+    private NavMeshAgent agent;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        agent.updatePosition = false;
+        agent.updateRotation = false;
+        //agent.destination = goal.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(goal.position);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = agent.velocity;
+        agent.nextPosition = rb.position;
     }
 }
