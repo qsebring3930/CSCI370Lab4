af25f68446e92680362bc16f95d2065936564736
Will Harris <william.harris108@gmail.com>
2021-03-06 22:12:35 -0600

    Got a working NavMesh Agent
    NavMesh Agent works. May adjust it when tracks are made and the AI has to contend with a player

--- Assets/Scripts/aiMove.cs
+++ Assets/Scripts/aiMove.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class aiMove : MonoBehaviour
 {
+    public Transform goal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
     }
 
     // Update is called once per frame
