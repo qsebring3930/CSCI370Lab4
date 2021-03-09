8d6818e9186d31ee6c3af2fa0c16f0d508e980c0
Will Harris <william.harris108@gmail.com>
2021-03-09 12:26:58 -0600

    Working on position tracking

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,13 +7,14 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
 
     [Header("UI stuff")]
-    public GameObject startButton;
+    public GameObject startButton, canvas, events;
 
     [Header("Position stuff")]
     public GameObject[] racers;
+    public Transform line;
 
-    public GameObject canvas;
-    public GameObject events;
+
+    private int playerpos = 0;
 
 
     private void Awake()
@@ -34,27 +35,38 @@ public class GameManager : MonoBehaviour
         
     }
 
-    //public IEnumerator updateRacerPos(GameObject[] r)
-    //{
-      //Coroutine here that checks the position of each racer and sorts them
-      //in an array according to how close they are to the finish line or checkpoint
-    //}
+    //Coroutine here that checks the position of each racer and sorts them
+    //in an array according to how close they are to the finish line or checkpoint
+    public IEnumerator updateRacerPos(GameObject[] r)
+    {
+        Vector3 localPos;
+        for(int i = 0; i <= r.Length; i++)
+        {
+            localPos = r[i].transform.position - line.position;
+            for(int j = 0; j <= r.Length; j++)
+            {
+                if(racers[j] == null)
+                {
+                    racers[j] = r[i];
+                    break;
+                }
+            }
+        }
+
+    }
 
 
     //Coroutine or something that routinely checks the list of racers
     //and returns index+1 as the position of the player racer
-    public int getRacerPos(string name)
+    public IEnumerator getRacerPos(string name)
     {
-        int pos = 0;
-        for(int i = 0; i < racers.Length; i++)
+        for(int i = 0; i <= racers.Length; i++)
         {
             if(racers[i].name == name)
             {
-                pos = i+1;
-                break;
+                yield return playerpos = i+1;
             }
         }
-        return pos;
     }
 
     // Update is called once per frame
