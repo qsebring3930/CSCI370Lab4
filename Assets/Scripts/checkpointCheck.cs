590a3df05dd2b79335dcdcfc3f533843a7463e4d
Will Harris <william.harris108@gmail.com>
2021-03-11 21:05:20 -0600

    Started some tentative work on checkpoints

---
+++ Assets/Scripts/checkpointCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class checkpointCheck : MonoBehaviour
+{
+    public Transform n;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "AI")
+        {
+            collision.gameObject.goal == n;
+        }
+    }
+}
